0220921a19679672c74d3161a609ef10715fcba9
Matheus Marques <matheusmarquesoliv@gmail.com>
2025-11-05 22:37:46 -0300

    Initial Commit

---
+++ src/main/java/com/matheusmarques/neopdv/NeoPdvApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmarques.neopdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoPdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusmarques/neopdv/NeoPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmarques.neopdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
