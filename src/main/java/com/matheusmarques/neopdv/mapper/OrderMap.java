cbe153befc74dd1f96c3ec0155f0c4a654a52e74
Matheus Marques <matheusmarquesoliv@gmail.com>
2025-11-15 14:34:44 -0300

    fix: Update Order mapping method to be static

--- src/main/java/com/matheusmarques/neopdv/mapper/OrderMap.java
+++ src/main/java/com/matheusmarques/neopdv/mapper/OrderMap.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class OrderMap {
-    public Order map(SalesStartRequest request){
+    public static Order map(SalesStartRequest request){
         Order order = new Order();
         order.setOwner(request.owner());
         order.setTableNumber(request.tableNumber());
